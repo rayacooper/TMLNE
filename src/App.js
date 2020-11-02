fcba7b6f4c01cc50dde7e385b5997a7917ab0505
rayacooper <47796461+rayacooper@users.noreply.github.com>
2020-11-02 15:57:39 -0700

    Added basic component files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import {BrowserRouter as Router} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+
+      </Router>
     </div>
   );
 }
