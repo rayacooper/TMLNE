e0b11fab516bc3a5099efd48a76fba3bc3647ff7
rayacooper <47796461+rayacooper@users.noreply.github.com>
2020-12-01 21:17:02 -0700

    Basic Server Build

---
+++ src/Components/Login/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+require('dotenv').config();
+
+const app = express();
+
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(cors());
+
+const port = process.env.PORT;
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+})
