2d0f55aac769998a026d7ff4c429a6378c941d4d
rayacooper <47796461+rayacooper@users.noreply.github.com>
2021-02-08 18:15:36 -0700

    Server

--- server.js
+++ server.js
@@ -1,32 +1,16 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const mysql = require('mysql');
+const massive = require('massive');
 
 require('dotenv').config();
-
-// const sqlConnection = mysql.createConnection({
-//     host: "",
-//     port: process.env.PORT,
-//     user: "",
-//     password: process.env.MYSQLSECRET,
-//     database: ""
-// })
-
-// connection.connect(function(err) {
-//     if (err) {
-//       console.error('error connecting: ' + err.stack);
-//       return;
-//     }
-   
-//     console.log('connected as id ' + connection.threadId);
-//   });
 
 const app = express();
 
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(cors());
+
 
 // endpoints
 
